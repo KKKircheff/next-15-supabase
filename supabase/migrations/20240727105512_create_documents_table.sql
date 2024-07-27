dfdbd034747c8e7c3c66ab6195a70e6358ecc782
Kiril Kirchev <102865219+KKKircheff@users.noreply.github.com>
2024-07-27 15:53:50 +0200

    supabase@beta &migrations

--- supabase/migrations/20240727105512_create_documents_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE extension IF NOT EXISTS vector WITH SCHEMA extensions;
-
-CREATE TABLE documents (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    body TEXT NOT NULL,
-    embedding VECTOR(384)
-);
-
